610683d937c6a85fb6050c9e1cb83f018be412db
anumarisheshankkumar-del <anumarisheshankkumar@gmail.com>
2025-12-10 18:52:22 -0600

    Add files via upload

---
+++ SumFirst10Numbers.java
@@ -0,0 +1,13 @@
+public class SumFirst10Numbers {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 10) {   
+            sum = sum + i;
+            i = i + 1;
+        }
+
+        System.out.println("Sum of first 10 numbers = " + sum);
+    }
+}
